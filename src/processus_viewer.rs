844a944b92427270a3145d16cafb24f63b9ea20d
Guillaume Gomez <guillaume1.gomez@gmail.com>
2015-05-23 14:32:48 +0200

    Add signal handling

--- src/processus_viewer.rs
+++ src/processus_viewer.rs
@@ -5,7 +5,7 @@ extern crate gtk;
 extern crate glib;
 extern crate sysinfo;
 
-use gtk::{WindowTrait, ContainerTrait, WidgetTrait};
+use gtk::{WindowTrait, ContainerTrait, WidgetTrait, ButtonSignals};
 use glib::{Type, timeout};
 use sysinfo::*;
 use gtk::signal::Inhibit;
@@ -41,12 +41,12 @@ fn create_and_fill_model(list_store: &mut gtk::ListStore, pid: i64, name: &str, 
     list_store.set_string(&top_level, 3, &format!("{}", memory));
 }
 
-fn update_window(w: &mut (&mut gtk::ListStore, &mut sysinfo::System)) -> i32 {
+fn update_window(w: &mut (&mut gtk::ListStore, Rc<RefCell<sysinfo::System>>)) -> i32 {
     let mut model = w.0.get_model().unwrap();
     let mut iter = gtk::TreeIter::new();
 
-    w.1.refresh();
-    let mut entries : VecMap<Processus> = (*w.1.get_processus_list()).clone();
+    (*w.1.borrow_mut()).refresh();
+    let mut entries : VecMap<Processus> = ((*w.1.borrow()).get_processus_list()).clone();
     let mut nb = model.iter_n_children(None);
     let mut i = 0;
 
@@ -85,7 +85,8 @@ fn main() {
     gtk::init();
 
     let mut window = gtk::Window::new(gtk::WindowType::TopLevel).unwrap();
-    let mut sys = sysinfo::System::new();
+    let mut sys : Rc<RefCell<sysinfo::System>> = Rc::new(RefCell::new(sysinfo::System::new()));
+    let mut sys1 = sys.clone();
 
     window.set_title("Processus viewer");
     window.set_window_position(gtk::WindowPosition::Center);
@@ -99,6 +100,9 @@ fn main() {
     let mut scroll = gtk::ScrolledWindow::new(None, None).unwrap();
     let mut current_pid : Rc<RefCell<Option<String>>> = Rc::new(RefCell::new(None));
     let mut current_pid1 = current_pid.clone();
+    let mut current_pid2 = current_pid.clone();
+    let mut kill_button : Rc<RefCell<gtk::Button>> = Rc::new(RefCell::new(gtk::Button::new_with_label("End task").unwrap()));
+    let mut kill_button1 = kill_button.clone();
 
     scroll.set_min_content_height(800);
     scroll.set_min_content_width(600);
@@ -115,8 +119,8 @@ fn main() {
     }
 
     let mut list_store = gtk::ListStore::new(&[Type::String, Type::String, Type::String, Type::String]).unwrap();
-    sys.refresh();
-    for (_, pro) in sys.get_processus_list() {
+    (*sys.borrow_mut()).refresh();
+    for (_, pro) in (*sys.borrow()).get_processus_list() {
         create_and_fill_model(&mut list_store, pro.pid, &pro.name, pro.cpu_usage, pro.memory);
     }
 
@@ -124,25 +128,55 @@ fn main() {
     left_tree.set_headers_visible(true);
     scroll.add(&left_tree);
     let mut vertical_layout = gtk::Box::new(gtk::Orientation::Vertical, 0).unwrap();
-    let mut kill_button = gtk::Button::new_with_label("End task").unwrap();
 
     left_tree.connect_cursor_changed(move |tree_view| {
-        let mut path = gtk::TreePath::new().unwrap();
+        match tree_view.get_selection() {
+            Some(selection) => {
+                let model = tree_view.get_model().unwrap();
+                let mut iter = gtk::TreeIter::new();
+
+                if selection.get_selected(&model, &mut iter) {
+                    let pid = model.get_value(&iter, 0).get_string();
+                    let mut tmp = current_pid1.borrow_mut();
+                    *tmp = pid;
+                }
+                (*kill_button1.borrow_mut()).set_sensitive((*current_pid.borrow()).is_some());
+            }
+            None => {
+                let mut tmp = current_pid1.borrow_mut();
+                *tmp = None;
+            }
+        }
+        /*let mut path = gtk::TreePath::new().unwrap();
+        println!("{:?}", path.unwrap_pointer());
         tree_view.get_cursor(Some(&mut path), None);
-        let mut iter = gtk::TreeIter::new();
+        println!("{:?}", path.unwrap_pointer());
         let model = tree_view.get_model().unwrap();
 
         if model.get_iter(&mut iter, &path) {
             let mut tmp = current_pid1.borrow_mut();
             *tmp = Some(model.get_value(&iter, 0).get_string().unwrap());
         }
-        println!("current pid : {:?}", current_pid);
+        println!("current pid : {:?}", current_pid);*/
     });
-    kill_button.set_sensitive(false);
-    timeout::add(1500, update_window, &mut (&mut list_store, &mut sys));
+    (*kill_button.borrow_mut()).set_sensitive(false);
+    (*kill_button.borrow_mut()).connect_clicked(move |_| {
+        let tmp = (*current_pid2.borrow_mut()).is_some() ;
+
+        if tmp {
+            let s = (*current_pid2.borrow()).clone();
+            match (*sys.borrow_mut()).get_processus(i64::from_str(&s.unwrap()).unwrap()) {
+                Some(p) => {
+                    p.kill(Signal::Kill);
+                },
+                None => {}
+            };
+        }
+    });
+    timeout::add(1500, update_window, &mut (&mut list_store, sys1.clone()));
 
     vertical_layout.add(&scroll);
-    vertical_layout.add(&kill_button);
+    vertical_layout.add(&(*kill_button.borrow_mut()));
     window.add(&vertical_layout);
     window.show_all();
     gtk::main();
