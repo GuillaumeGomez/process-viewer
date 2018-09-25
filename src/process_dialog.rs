322f1d9c20587cf8d1bb6f33ce7b5d540f0d6a12
Guillaume Gomez <guillaume1.gomez@gmail.com>
2018-09-26 00:11:34 +0200

    Improve dialog again

--- src/process_dialog.rs
+++ src/process_dialog.rs
@@ -1,5 +1,7 @@
 use glib::object::Cast;
-use gtk::{self, BoxExt, ButtonExt, ContainerExt, DialogExt, LabelExt, ScrolledWindowExt};
+use gtk::{
+    self, AdjustmentExt, BoxExt, ButtonExt, ContainerExt, DialogExt, LabelExt, ScrolledWindowExt
+};
 use gtk::{WidgetExt, GtkWindowExt};
 use pango;
 use sysinfo::{self, Pid, ProcessExt};
@@ -17,13 +19,11 @@ pub struct ProcDialog {
 
 impl ProcDialog {
     pub fn update(&self, process: &sysinfo::Process, running_since: u64, start_time: u64) {
-        self.working_directory.set_text(&format!("current working directory: {}",
-                                                 process.cwd().display()));
-        self.memory_usage.set_text(&format!("memory usage: {}",
-                                            format_number(process.memory() << 10))); // * 1_024
-        self.cpu_usage.set_text(&format!("cpu usage: {:.1}%", process.cpu_usage()));
+        self.working_directory.set_text(&process.cwd().display().to_string());
+        self.memory_usage.set_text(&format_number(process.memory() << 10)); // * 1_024
+        self.cpu_usage.set_text(&format!("{:.1}%", process.cpu_usage()));
         let running_since = compute_running_since(process, start_time, running_since);
-        self.run_time.set_text(&format!("Running since: {}", format_time(running_since)));
+        self.run_time.set_text(&format_time(running_since));
     }
 }
 
@@ -56,14 +56,28 @@ fn format_time(t: u64) -> String {
             t % 60)
 }
 
-fn create_and_add_new_label(scroll: &gtk::Box, text: &str) -> gtk::Label {
-    let label = gtk::Label::new(text);
-    label.set_selectable(true);
+fn create_and_add_new_label(scroll: &gtk::Box, title: &str, text: &str) -> gtk::Label {
+    let horizontal_layout = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+
+    horizontal_layout.set_margin_top(5);
+    horizontal_layout.set_margin_bottom(5);
+    horizontal_layout.set_margin_right(5);
+    horizontal_layout.set_margin_left(5);
+
+    let label = gtk::Label::new(None);
     label.set_justify(gtk::Justification::Left);
-    label.set_line_wrap(true);
-    label.set_line_wrap_mode(pango::WrapMode::Char);
-    scroll.add(&label);
-    label
+    label.set_markup(&format!("<b>{}:</b>", title));
+
+    let text = gtk::Label::new(text);
+    text.set_selectable(true);
+    text.set_justify(gtk::Justification::Left);
+    text.set_line_wrap(true);
+    text.set_line_wrap_mode(pango::WrapMode::Char);
+
+    horizontal_layout.add(&label);
+    horizontal_layout.add(&text);
+    scroll.add(&horizontal_layout);
+    text
 }
 
 fn compute_running_since(
@@ -100,47 +114,51 @@ pub fn create_process_dialog(
 
     let labels = gtk::Box::new(gtk::Orientation::Vertical, 0);
 
-    create_and_add_new_label(&labels, &format!("name: {}", process.name()));
-    create_and_add_new_label(&labels, &format!("pid: {}", process.pid()));
+    create_and_add_new_label(&labels, "name", process.name());
+    create_and_add_new_label(&labels, "pid", &process.pid().to_string());
     let memory_usage = create_and_add_new_label(&labels,
-                                                &format!("memory usage: {}",
-                                                         format_number(process.memory() << 10)));
+                                                "memory usage",
+                                                &format_number(process.memory() << 10));
     let cpu_usage = create_and_add_new_label(&labels,
-                                             &format!("cpu usage: {:.1}%", process.cpu_usage()));
+                                             "cpu usage",
+                                             &format!("{:.1}%", process.cpu_usage()));
     let run_time = create_and_add_new_label(&labels,
-                                            &format!("Running since: {}",
-                                                     format_time(running_since)));
-    create_and_add_new_label(&labels, &format!("command: {:?}", process.cmd()));
-    create_and_add_new_label(&labels, &format!("executable path: {}", process.exe().display()));
-    let working_directory = create_and_add_new_label(&labels,
-                                                     &format!("current working directory: {}",
-                                                              process.cwd().display()));
-    create_and_add_new_label(&labels, &format!("root directory: {}", process.root().display()));
-    let mut text = format!("environment:");
+                                            "Running since",
+                                            &format_time(running_since));
+    create_and_add_new_label(&labels, "command", &format!("{:?}", process.cmd()));
+    create_and_add_new_label(&labels, "executable path", &process.exe().display().to_string());
+    let working_directory = create_and_add_new_label(&labels, "current working directory",
+                                                     &process.cwd().display().to_string());
+    create_and_add_new_label(&labels, "root directory", &process.root().display().to_string());
+    let mut text = String::with_capacity(100);
     for env in process.environ() {
         text.push_str(&format!("\n{:?}", env));
     }
-    create_and_add_new_label(&labels, &text);
+    create_and_add_new_label(&labels, "environment", &text);
 
     scroll.add(&labels);
     vertical_layout.pack_start(&scroll, true, true, 0);
     vertical_layout.pack_start(&close_button, false, true, 0);
-    vertical_layout.set_spacing(10);
     area.pack_start(&vertical_layout, true, true, 0);
     // To silence the annoying warning:
     // "(.:2257): Gtk-WARNING **: Allocating size to GtkWindow 0x7f8a31038290 without
     // calling gtk_widget_get_preferred_width/height(). How does the code know the size to
     // allocate?"
     popup.get_preferred_width();
-    popup.set_size_request(500, 700);
+    popup.set_size_request(500, 500);
 
     let popup = popup.upcast::<gtk::Window>();
-    popup.set_resizable(false);
+    popup.set_resizable(true);
     popup.show_all();
     let pop = popup.clone();
     close_button.connect_clicked(move |_| {
         pop.destroy();
     });
+
+    if let Some(adjust) = scroll.get_vadjustment() {
+        adjust.set_value(0.);
+        scroll.set_vadjustment(&adjust);
+    }
     ProcDialog {
         working_directory,
         memory_usage,
