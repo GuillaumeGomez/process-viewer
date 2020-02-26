b52a4d8f7b39ee697874aaeb08e851c961bbdbf6
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-02-26 17:47:31 +0100

    More information for networks

--- src/display_network.rs
+++ src/display_network.rs
@@ -24,6 +24,14 @@ struct NetworkData {
     in_usage: gtk::Label,
     // network out usage
     out_usage: gtk::Label,
+    // network income packets
+    income_packets: gtk::Label,
+    // network outcome packets
+    outcome_packets: gtk::Label,
+    // network income errors
+    income_errors: gtk::Label,
+    // network outcome errors
+    outcome_errors: gtk::Label,
 }
 
 pub struct Network {
@@ -99,6 +107,19 @@ impl Network {
                 history.data[1].move_start();
                 *history.data[1].get_mut(0).expect("cannot get data 1") = data.get_outcome() as f64;
                 history.invalidate();
+
+                network
+                    .income_packets
+                    .set_text(&better_number(data.get_total_packets_income()));
+                network
+                    .outcome_packets
+                    .set_text(&better_number(data.get_total_packets_outcome()));
+                network
+                    .income_errors
+                    .set_text(&better_number(data.get_total_errors_income()));
+                network
+                    .outcome_errors
+                    .set_text(&better_number(data.get_total_errors_outcome()));
             }
         }
     }
@@ -108,6 +129,38 @@ impl Network {
         for elem in elems.iter() {
             elem.check_box.set_active(active);
         }
+    }
+}
+
+fn create_non_graph_labels(label_text: &str, text: &str, non_graph_layout: &gtk::Box) -> gtk::Label {
+    let label = gtk::Label::new(Some(text));
+    let horizontal_layout = gtk::Box::new(gtk::Orientation::Horizontal, 10);
+    horizontal_layout.pack_start(&gtk::Label::new(Some(label_text)), true, false, 0);
+    horizontal_layout.pack_start(&label, true, false, 0);
+    horizontal_layout.set_homogeneous(true);
+    non_graph_layout.add(&horizontal_layout);
+    label
+}
+
+fn better_number(mut f: u64) -> String {
+    if f < 1000 {
+        f.to_string()
+    } else {
+        let mut s = String::new();
+        let mut count = 0;
+        while f > 0 {
+            if !s.is_empty() && count % 3 == 0 {
+                s.push(' ');
+            }
+            s.push((((f % 10) as u8) + b'0') as char);
+            f /= 10;
+            count += 1;
+        }
+        {
+            let vec = unsafe { s.as_mut_vec() };
+            vec.reverse();
+        }
+        s
     }
 }
 
@@ -160,29 +213,26 @@ fn create_network_interface(layout: &gtk::Box, name: String, settings: &Settings
     let non_graph_layout = gtk::Box::new(gtk::Orientation::Vertical, 0);
     let check_box = display_sysinfo::create_header(&name, &layout, settings.display_graph);
     // input data
-    let in_usage = gtk::Label::new(Some(&format_number(0)));
-    let horizontal_layout = gtk::Box::new(gtk::Orientation::Horizontal, 10);
-    horizontal_layout.pack_start(&gtk::Label::new(Some("Input data")), true, false, 0);
-    horizontal_layout.pack_start(&in_usage, true, false, 0);
-    horizontal_layout.set_homogeneous(true);
-    non_graph_layout.add(&horizontal_layout);
+    let in_usage = create_non_graph_labels("Input data", &format_number(0), &non_graph_layout);
     history.push(
         RotateVec::new(iter::repeat(0f64).take(61).collect()),
         "Input data",
         None,
     );
     // output data
-    let out_usage = gtk::Label::new(Some(&format_number(0)));
-    let horizontal_layout = gtk::Box::new(gtk::Orientation::Horizontal, 10);
-    horizontal_layout.pack_start(&gtk::Label::new(Some("Output data")), true, false, 0);
-    horizontal_layout.pack_start(&out_usage, true, false, 0);
-    horizontal_layout.set_homogeneous(true);
-    non_graph_layout.add(&horizontal_layout);
+    let out_usage = create_non_graph_labels("Output data", &format_number(0), &non_graph_layout);
     history.push(
         RotateVec::new(iter::repeat(0f64).take(61).collect()),
         "Output data",
         None,
     );
+    // packets
+    let income_packets = create_non_graph_labels("Total income packets", "0", &non_graph_layout);
+    let outcome_packets = create_non_graph_labels("Total outcome packets", "0", &non_graph_layout);
+    // errors
+    let income_errors = create_non_graph_labels("Total income errors", "0", &non_graph_layout);
+    let outcome_errors = create_non_graph_labels("Total outcome errors", "0", &non_graph_layout);
+
     layout.add(&non_graph_layout);
     history.attach_to(&layout);
     history.area.set_margin_bottom(20);
@@ -203,5 +253,9 @@ fn create_network_interface(layout: &gtk::Box, name: String, settings: &Settings
         in_usage,
         out_usage,
         updated: false,
+        income_packets,
+        outcome_packets,
+        income_errors,
+        outcome_errors,
     }
 }
