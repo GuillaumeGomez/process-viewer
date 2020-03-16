58e6d7a81c0b8d2610c4604ea145c463c0fbd297
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2020-03-16 13:54:27 +0000

    cargo fmt

--- src/process_viewer.rs
+++ src/process_viewer.rs
@@ -366,11 +366,9 @@ fn build_ui(application: &gtk::Application) {
     menu.append(Some("Launch new executable"), Some("app.new-task"));
     menu.append(Some("Quit"), Some("app.quit"));
     let quit = gio::SimpleAction::new("quit", None);
-    quit.connect_activate(
-        clone!(@weak application => move |_,_| {
-            application.quit();
-        }),
-    );
+    quit.connect_activate(clone!(@weak application => move |_,_| {
+        application.quit();
+    }));
     application.set_accels_for_action("app.quit", &["<Primary>Q"]);
 
     settings_menu.append(Some("Display temperature in °F"), Some("app.temperature"));
